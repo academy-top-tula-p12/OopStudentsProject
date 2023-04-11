0ca5be258eee076b81b72294b4cb94247e0484ae
maxefimov <efimov.academy.top@gmail.com>
2023-04-11 21:22:27 +0300

    11-04-23

--- OopStudentsProject/OopStudentsProject.cpp
+++ OopStudentsProject/OopStudentsProject.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+
 class Student
 {
     string name;
@@ -21,6 +22,22 @@ public:
 
     void SetAge(int age) { this->age = age; }
     int GetAge() { return age; }
+
+    friend bool operator<(const Student& s1, const Student& s2);
+};
+
+bool operator<(const Student& s1, const Student& s2)
+{
+    return s1.name < s2.name;
+}
+
+class ComparerAge
+{
+public:
+    bool operator()(Student s1, Student s2)
+    {
+        return s1.GetAge() < s2.GetAge();
+    }
 };
 
 class Group
@@ -29,6 +46,8 @@ class Group
     Student* students;
 public:
     Group() : size{ 0 }, students{ nullptr } {}
+
+    int Size() { return size; }
     
     void Add(Student student)
     {
@@ -63,12 +82,83 @@ public:
     {
         this->Add(student);
     }
+
+    void Sort()
+    {
+        for (int i = 0; i < size; i++)
+        {
+            for(int j = size - 1; j > i; j--)
+                if (students[j] < students[j - 1])
+                {
+                    Student temp = students[j];
+                    students[j] = students[j - 1];
+                    students[j - 1] = temp;
+                }
+        }
+    }
+
+    void Sort(ComparerAge operation)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = size - 1; j > i; j--)
+                if (operation(students[j], students[j - 1]))
+                {
+                    Student temp = students[j];
+                    students[j] = students[j - 1];
+                    students[j - 1] = temp;
+                }
+        }
+    }
+
+    Student& At(int index)
+    {
+        return students[index];
+    }
 };
+
+class Accumulator
+{
+    int acc;
+public:
+    Accumulator() : Accumulator(0) {}
+    Accumulator(int init) : acc{ init } {}
+    int operator()(int number)
+    {
+        return acc += number;
+    }
+};
+
+void GroupPrint(Group group)
+{
+    for (int i = 0; i < group.Size(); i++)
+        cout << group.At(i).GetName() 
+             << " " 
+             << group.At(i).GetAge() << "\n";
+
+    cout << "\n";
+}
 
 int main()
 {
-    Group group;
-    group.Add(*(new Student("Bob", 23)));
+    /*Accumulator acc;
+    cout << acc(10) << "\n";
+    cout << acc(30) << "\n";*/
 
-    group += *(new Student("Joe", 34));
+    Group group;
+    group.Add(Student("Bob", 23));
+    group += Student("Joe", 34);
+    group += Student("Ben", 19);
+    group += Student("Tom", 26);
+    group += Student("Sam", 32);
+
+    GroupPrint(group);
+
+    group.Sort();
+
+    GroupPrint(group);
+
+    group.Sort(ComparerAge());
+
+    GroupPrint(group);
 }
